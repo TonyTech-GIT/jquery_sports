2506e437bcae78c3e239b4d0c6460d953281a2b7
TonyTech <toomgbebu@gmail.com>
2024-07-01 16:42:46 +0100

    menu functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
-    // alert('hellooooo')
+    $('.header_container-menuBtn').click(function () {
+
+        // When the menu button is first clicked...
+        $('.mobile_menu-container').toggleClass('show');
+        $('.overlay').toggle();
+
+        $('.header_container-menuBtn').hide()
+        $('.header_container-menuClose').show()
+    })
+
+
+
+    // When the closing menu button is clicked...
+    $('.header_container-menuClose').click(function () {
+        $('.header_container-menuBtn').show()
+        $('.header_container-menuClose').hide()
+
+        $('.mobile_menu-container').removeClass('show');
+        $('.overlay').toggle(); 
+    })
 });
 
-// console.log('hjuhkjkujhnk');
